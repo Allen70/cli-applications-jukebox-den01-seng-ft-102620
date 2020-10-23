38ed009b49dba6d1a2890aff424d7463b36ebe19
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-23 03:54:05 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -15,22 +15,23 @@ count = 1
  count+=1
  }
 end
-  
+
 def play (song)
   puts "Please enter a song name or number:"
   count = 1
   input = gets.strip
-  
   song.each{|key|
-  
   if input == "#{count}" || input == "#{key}"
     puts "Playing #{key}"
     return
   elsif key == "Amos Lee - Keep It Loose, Keep It Tight"
     puts "Invalid input, please try again"
   end
-
   count += 1
   }
+end
+
+def exit_jukebox
+  
   
 end
