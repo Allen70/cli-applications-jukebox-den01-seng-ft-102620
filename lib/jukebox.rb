560f48bd777c3ab674c43cc3d411f4a35039d625
Allen Richard Lea <allenlea2007@gmail.com>
2020-10-23 03:13:10 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -1,1 +1,25 @@
-# Add your code here
+require 'pry'
+def help 
+  
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
+
+def list (songs)
+songs.each{|songs|
+
+}
+end
+  
+def play (song)
+  
+  puts "Please enter a song name or number."
+  input = gets.strip
+ song.each{|key|
+ 
+ binding.pry
+ }
+end
